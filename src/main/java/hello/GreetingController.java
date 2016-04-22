1c899ca0a71eb6748d9771650121a278bb211222
Lyon-NEU <wanlplab@gmail.com>
2016-04-22 21:20:16 +0800

    first commit

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,32 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Value;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @Value("${name}")
+    private String name;
+    @Value("${age}")
+    private String age;
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+
+    @RequestMapping("/info")
+    public String userInfo(){
+     	String info=String.format("name: %s age: %s",name,age);
+     	return info;
+    	//return "Hello, Spirng Boot!"+name;
+    }
+}
