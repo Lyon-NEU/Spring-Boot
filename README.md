6b8953645fb2b6278a7f292dd1ecfddb654224d2
Lyon-NEU <wanlplab@gmail.com>
2016-05-10 23:12:07 +0800

    modify User

--- README.md
+++ README.md
@@ -23,3 +23,19 @@ spring.datasource.driver-class-name=com.mysql.jdbc.Driver <br>
             <artifactId>spring-boot-starter-jdbc</artifactId>
         </dependency>
 ```
+
+## 日志
+    
+Spring Boot 是通过调用[Commons Logging](http://commons.apache.org/proper/commons-logging/)来实现内部日志功能。用户可根据自身需要来实现底层的接口。默认的配置为`Java Util Logging, Log4J,Log4J2`和`Logback`
+如果选择`Starter POMS`，系统会默认选择Logback
+
+### 日志格式
+
+默认的日志输出格式像下面这样：
+``` log
+2014-03-05 10:57:51.112  INFO 45469 --- [           main] org.apache.catalina.core.StandardEngine  : Starting Servlet Engine: Apache Tomcat/7.0.52
+2014-03-05 10:57:51.253  INFO 45469 --- [ost-startStop-1] o.a.c.c.C.[Tomcat].[localhost].[/]       : Initializing Spring embedded WebApplicationContext
+2014-03-05 10:57:51.253  INFO 45469 --- [ost-startStop-1] o.s.web.context.ContextLoader            : Root WebApplicationContext: initialization completed in 1358 ms
+2014-03-05 10:57:51.698  INFO 45469 --- [ost-startStop-1] o.s.b.c.e.ServletRegistrationBean        : Mapping servlet: 'dispatcherServlet' to [/]
+2014-03-05 10:57:51.702  INFO 45469 --- [ost-startStop-1] o.s.b.c.embedded.FilterRegistrationBean  : Mapping filter: 'hiddenHttpMethodFilter' to:
+```
--- src/main/java/hello/MyBean.java
+++ src/main/java/hello/MyBean.java
@@ -1,8 +1,10 @@
-package hello;
+package com.asit.gdm.service;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+
+import com.asit.gdm.domain.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -12,25 +14,48 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyBean{
 
-	private final JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
-	@Autowired
-	public MyBean(JdbcTemplate jdbcTemplate){
-		this.jdbcTemplate=jdbcTemplate;
-	}
+    @Autowired
+    public MyBean(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate=jdbcTemplate;
+    }
 
     public List<User> getList(){
-    String sql = "select id,name from users";
-    return (List<User>) jdbcTemplate.query(sql, new RowMapper<User>(){
+        String sql = "select id,name from users";
+        return (List<User>) jdbcTemplate.query(sql, new RowMapper<User>(){
 
-        @Override
-        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-            User user = new User();
-            user.setID(rs.getInt("id"));
-            user.setName(rs.getString("name"));
-            return user;
-        }
+            @Override
+            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                User user = new User();
+                user.setID(rs.getInt("id"));
+                user.setName(rs.getString("name"));
+                return user;
+            }
 
-    });
+        });
     }
+    //根据姓名来查询，返回列表
+    public List<User> getUser(String name){
+        String sql = "select * from users where name='"+name+"'";
+        return (List<User>) jdbcTemplate.query(sql, new RowMapper<User>(){
+
+            @Override
+            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                User user = new User();
+                user.setID(rs.getInt("id"));
+                user.setName(rs.getString("name"));
+                return user;
+            }
+
+        });
+    }
+    //根据ID查询，返回惟一结果 
+    public User getUserID(int id){
+        return jdbcTemplate.queryForObject("select * from users where id= ?",new Object[]{id},User.class);
+    }
+    //新建用户
+    // public String save(User user){
+    //     return jdbcTemplate.update("insert into users(id,name) values(?,?)",user.getID(),user.getName());
+    // }
 }
